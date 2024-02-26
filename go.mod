3e523f9983318b5422b1a4071f2b30eb87f60d06
vartte <vartan.spb@gmail.com>
2024-02-26 21:01:36 +0300

    дошел до getValueByIndex

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/artsarhub/linked-list-practice
 
-go 1.21.1
+go 1.21
--- main.go
+++ main.go
@@ -26,60 +26,83 @@ func printList(list *Node) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-// pushFront добавляет новый элемент в начало списка
+pushFront добавляет новый элемент в начало списка
 func pushFront(list *Node, value int) {
-		tmpList:=list
-		//tmpList.next=tmpList
-
-		for list.Next() != nil {
-			tmpList.next=tmpList
-		}
-
-		// изм. linkedList
-		// tmpList.next=&Node{
-		// 	value: value,
-		// 	next: list.next,
-		// }
-
-		// не изм linkedList
-		tmpList=&Node{
-			value: value,
-			next:  &Node{
-				value: list.Value(),
-				next:  list.Next(),
-			},
-		}
-		//list=tmpList
-		//tmpList.Value()=value
-
-
+			item:=&Node{
+			value: list.value,
+			next: list.next,
+			}
+			list.value=value
+			list.next=item
 	//panic("not implemented")
 }
 
-// // pushBack добавляет новый элемент в конец списка
-// func pushBack(list *Node, value int) {
-// 	panic("not implemented")
-// }
+// pushBack добавляет новый элемент в конец списка
+func pushBack(list *Node, value int) {
+			thirdItem:=&Node{
+				value: value,
+				next: nil,
+			}
+			secondItem:=list
+			for secondItem.next!=nil{
+				secondItem=secondItem.next
+			}
+			secondItem.next=thirdItem
+
+	// panic("not implemented")
+}
 
 // // count возвращает кол-во элементов в списке
-// func count(list *Node) int {
-// 	panic("not implemented")
-// }
+	 func count(list *Node) int {
+		count:=0
+		item:=list
+		for item.next!=nil{
+			item=item.next
+			count++
+		}
+		return count
 
-// // popFront возвращает значение первого элемента и удаляет его из списка
-// func popFront(list *Node) int {
-// 	panic("not implemented")
-// }
+	// 	panic("not implemented")
+ }
+
+// popFront возвращает значение первого элемента и удаляет его из списка
+func popFront(list *Node) int {
+	item:=list
+	firstValue:=list.value
+	list.value=item.next.value
+	list.next=item.next.next
+	
+
+	return firstValue 
+	//panic("not implemented")
+}
 
 // // popBack возвращает значение последнего элемента и удаляет его из списка
-// func popBack(list *Node) int {
-// 	panic("not implemented")
-// }
+func popBack(list *Node) int {
+		secondItem:=list
+		//for secondItem.next!=nil{
+			for i:=0;i<count(list)-1;i++{
+			secondItem=secondItem.next
+		}
+		lastValue:=secondItem.next.value
+		secondItem.next=nil
+		return lastValue
+	//panic("not implemented")
+}
 
-// // isValueInList ищет значение в списке и возвращает true, если оно найдено, в ином случае - false
-// func isValueInList(list *Node, value int) bool {
-// 	panic("not implemented")
-// }
+// isValueInList ищет значение в списке и возвращает true, если оно найдено, в ином случае - false
+func isValueInList(list *Node, value int) bool {
+	secondItem:=list
+
+	for secondItem.next!=nil{
+		if secondItem.value==value{
+			return true
+		}
+		secondItem=secondItem.next
+	}
+	//panic("not implemented")
+	return false
+}
 
 // // getValueByIndex возвращает значение из списка по индексу, если оно есть, в ином случае - error("index out of range")
 // func getValueByIndex(list *Node, index int) (int, error) {
@@ -106,10 +129,16 @@ func main() {
 		value: 0,
 		next:  nil,
 	}
-	list:=[]int{1,2,3,4,5}
+	listValue:=[]int{1,2,3,4,5}
 
-	for _,v:= range list{
+	for _,v:= range listValue{
 		pushFront(linkedList,v)
 	}
+	for _,v:= range listValue{
+			pushBack(linkedList,v)
+		 }
+	fmt.Println(popFront(linkedList))
+	fmt.Println(popBack(linkedList))
+	fmt.Println(isValueInList(linkedList,3))
 	printList(linkedList)
 }
